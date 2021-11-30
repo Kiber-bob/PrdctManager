e418e06f95e291cf983b00d6cc4577bdf0045f37
kiber-bob <dn.ermak@list.ru>
2021-11-30 11:39:52 +0300

    src/ added

--- src/main/java/ru/netology/domain/Book.java
+++ src/main/java/ru/netology/domain/Book.java
@@ -15,10 +15,6 @@ public class Book extends Product {
 
     private String author;
 
-    public Book() {
-        super();
-    }
-
 
     public Book(int id, String name, int price, String author) {
         super(id, name, price);
--- src/main/java/ru/netology/domain/Smartphone.java
+++ src/main/java/ru/netology/domain/Smartphone.java
@@ -16,9 +16,6 @@ public class Smartphone extends Product {
 
     private String manufacturer;
 
-    public Smartphone() {
-        super();
-    }
 
     public Smartphone(int id, String name, int price, String manufacturer) {
         super(id, name, price);
